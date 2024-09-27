065ae6c6ac7bb76304f014a904c4ac8167246fc5
liangsy <chenqiang@163.com>
2024-09-27 17:59:32 +0800

    测试前端页面提交

---
+++ src/views/business/front/index.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('Hello World')
+const tableData = [
+  {
+    id: 12983718390123,
+    date: '2024-09-26',
+    name: 'tom',
+    address: 'shhh'
+  },
+  {
+    id: 8762178301298763,
+    date: '2024-09-26',
+    name: 'jerry',
+    address: 'xshye'
+  }
+]
+
+function clickWarn(row) {
+  alert(row.id + "   " + row.name + "  " + row.address)
+}
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+  <el-table :data="tableData">
+    <el-table-column prop="date" label="日期" />
+    <el-table-column prop="name" label="姓名" />
+    <el-table-column prop="address" label="地址" />
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button size="small" @click="clickWarn(scope.row)">点击</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<style scoped lang="scss">
+
+</style>
