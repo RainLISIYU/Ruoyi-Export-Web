a8365a92ddc17f00d4378481de010bb5065774cf
liangsy <chenqiang@163.com>
2025-07-30 17:50:08 +0800

    文件地址修改，页面增加模板上传

--- src/api/business/verify.js
+++ src/api/business/verify.js
@@ -2,6 +2,10 @@ import request from '@/utils/request'
 
 export const url = '/admin/api/v1/verify'
 
+export const importDataUrl = url + '/importData'
+
+export const uploadTemplateUrl = url + '/uploadTemplate'
+
 export function getPage(query) {
     return request({
         url: url + '/page',
--- src/views/business/verify/index.vue
+++ src/views/business/verify/index.vue
@@ -8,6 +8,12 @@
             icon="Upload"
             @click="handleImport"
         >导入</el-button>
+        <el-button
+            type="info"
+            plain
+            icon="Upload"
+            @click="handleTemplate"
+        >模板上传</el-button>
       </el-col>
     </el-row>
     <el-col :span="20" :xs="24">
@@ -42,10 +48,11 @@
       />
     </el-col>
 
+    <!-- 导入框 -->
     <el-dialog :title="upload.title" v-model="upload.open" width="400px" append-to-body>
       <el-upload
           ref="uploadRef"
-          :limit="2"
+          :limit="1"
           accept=".xlsx, .xls"
           :headers="upload.headers"
           :action="upload.url"
@@ -59,9 +66,6 @@
         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
         <template #tip>
           <div class="el-upload__tip text-center">
-            <div class="el-upload__tip">
-              <el-checkbox v-model="upload.updateSupport" />是否更新已经存在的用户数据
-            </div>
             <span>仅允许导入xls、xlsx格式文件。</span>
             <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="">下载模板</el-link>
           </div>
@@ -74,13 +78,44 @@
         </div>
       </template>
     </el-dialog>
+
+    <!-- 模板上传 -->
+    <el-dialog :title="temUpload.title" v-model="temUpload.open" width="400px" append-to-body>
+      <el-upload
+          ref="temUploadRef"
+          :limit="1"
+          accept=".docx, .doc"
+          :headers="temUpload.headers"
+          :action="temUpload.url"
+          :disabled="temUpload.isUploading"
+          :on-progress="handleTemUploadProgress"
+          :on-success="handleTemSuccess"
+          :auto-upload="false"
+          drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <span>仅允许导入doc、docx格式文件。</span>
+            <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="">下载模板</el-link>
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitTemForm">确 定</el-button>
+          <el-button @click="temUpload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
 
 import { getToken } from "@/utils/auth";
-import { getPage } from '@/api/business/verify'
+import { getPage, uploadTemplateUrl, importDataUrl } from '@/api/business/verify'
 
 const { proxy } = getCurrentInstance();
 const verifyList = ref([])
@@ -102,7 +137,23 @@ const upload = reactive({
   // 设置上传的请求头部
   headers: { Authorization: "Bearer " + getToken() },
   // 上传的地址
-  url: import.meta.env.VITE_APP_BASE_API + "/admin/api/v1/verify/importData"
+  url: import.meta.env.VITE_APP_BASE_API + importDataUrl
+});
+
+/*** 模板上传参数 */
+const temUpload = reactive({
+  // 是否显示弹出层（用户导入）
+  open: false,
+  // 弹出层标题（用户导入）
+  title: "",
+  // 是否禁用上传
+  isUploading: false,
+  // 是否更新已经存在的用户数据
+  updateSupport: 0,
+  // 设置上传的请求头部
+  headers: { Authorization: "Bearer " + getToken() },
+  // 上传的地址
+  url: import.meta.env.VITE_APP_BASE_API + uploadTemplateUrl
 });
 
 const data = reactive({
@@ -139,8 +190,14 @@ function handleSelectionChange(selection) {
 
 /** 导入按钮操作 */
 function handleImport() {
-  upload.title = "用户导入";
+  upload.title = "数据导入";
   upload.open = true;
+}
+
+/** 模板导入按钮 */
+function handleTemplate() {
+  temUpload.title = "模板上传";
+  temUpload.open = true;
 }
 
 /**文件上传中处理 */
@@ -148,18 +205,35 @@ const handleFileUploadProgress = (event, file, fileList) => {
   upload.isUploading = true;
 }
 
+/**模板上传中处理 */
+const handleTemUploadProgress = (event, file, fileList) => {
+  temUpload.isUploading = true;
+}
+
 /** 文件上传成功处理 */
 const handleFileSuccess = (response, file, fileList) => {
   upload.open = false;
   upload.isUploading = false;
   proxy.$refs["uploadRef"].handleRemove(file);
-  proxy.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
-  getList();
+  proxy.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true })
+}
+
+const handleTemSuccess = (response, file, fileList) => {
+  temUpload.open = false
+  temUpload.isUploading = false
+  proxy.$refs["temUploadRef"].handleRemove(file)
+  proxy.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "上传结果", { dangerouslyUseHTMLString: true })
+  console.log(response.data)
 }
 
 /** 提交上传文件 */
 function submitFileForm() {
   proxy.$refs["uploadRef"].submit();
+}
+
+/** 模板上传 */
+function submitTemForm() {
+  proxy.$refs["temUploadRef"].submit();
 }
 
 query()
